d32a54a54742d67df3178751a66cf667c8d62ca9
Compiledjay <139014932+Compiledjay@users.noreply.github.com>
2024-05-20 23:18:58 -0700

    E.feat: add text type properties and conversion to htmlnode for textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,16 @@
+from htmlnode import (
+    LeafNode,
+)
+
+
+type_text   : str   = "text"
+type_bold   : str   = "bold"
+type_italic : str   = "italic"
+type_code   : str   = "code"
+type_link   : str   = "link"
+type_image  : str   = "image"
+
+
 class TextNode():
     def __init__(self, text: str, text_type: str, url: str = None) -> None:
         self.text       = text
@@ -16,3 +29,19 @@ class TextNode():
     
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
